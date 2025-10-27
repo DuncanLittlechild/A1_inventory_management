dd5b930aefcb6ded16bebd87c4d42f7711931fe3
DuncanLittlechild <177414121+DuncanLittlechild@users.noreply.github.com>
2025-10-27 15:29:00 +0000

    Add database to setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     name="A1_inventory_management",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    include_package_data=True,
+    package_data={
+        "A1_inventory_management": ["dbs/*.sql"],
+    },
 )
