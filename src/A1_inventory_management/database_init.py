7aafbeba142e41f3bba825cf400f2cfec6d484dc
DuncanLittlechild <177414121+DuncanLittlechild@users.noreply.github.com>
2025-10-27 14:17:30 +0000

    Revert "Move databases into package"
    This reverts commit 77f112973cc314f7e6bc47d5623a4b938985fbc9.

--- src/A1_inventory_management/database_init.py
+++ src/A1_inventory_management/database_init.py
@@ -9,7 +9,7 @@ from pathlib import Path
 #########################
 # If databases don't exist, initialise them from the db_sqlite_code file
 def initialiseDb():
-    path = Path(__file__).parent / "dbs/db_sqlite_code.sql"
+    path = Path(__file__).parent / "../../dbs/db_sqlite_code.sql"
     conn = sql.connect("dbs/stock_database.db")
     sqlScript = ""
     with open(path) as f:
